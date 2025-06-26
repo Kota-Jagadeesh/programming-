0248b8f94c1eaf6becfa81c86bfd42f6846b8b4b
VoidRaven <kota.jagadesh123@gmail.com>
2025-06-26 19:37:35 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,21 +1,11 @@
 n = int(input("number of terms:")) 
-
 a = 0 
-
 b = 1 
-
 if n >= 1: 
-
     print(a, end=" ") 
-
 if n >= 2: 
-
     print(b, end=" ") 
-
 for i in range(2, n): 
-
     next_term = a + b 
-
     print(next_term, end=" ") 
-
     a, b = b, next_term 
